5cc071b3f5931d90c0500ac08c4536b4220477dc
zen_leo <leo.in.empty@gmail.com>
2018-12-25 17:36:58 +0300

    Add styles and image

--- www/js/routes.js
+++ www/js/routes.js
@@ -14,14 +14,11 @@ routes = [
             pageInit: function (e, page) {
                 $$('.save-truck-data').on('click', function(){
                     let formData = app.form.convertToData('#truckForm');
-                    /*console.log(JSON.stringify(formData));
-                    console.log(page.route.params.id);*/
+                    console.log(formData);
                     app.request({
                         url: 'http://rsprm.ru/test/truck/'.concat(page.route.params.id),
                         method: 'PATCH',
-                        data: formData,
-                        crossDomain: true
-
+                        data: formData
                     });
                 });
             }
